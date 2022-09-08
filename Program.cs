be3f723c74d76da3b0b7d24c2a4297e7bcf1db9b
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:13:33 -0600

    finished step: 12

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿//WorkWithIntegers();
 OrderPrecedence();
+TestLimits();
 
 void WorkWithIntegers()
 {
@@ -45,17 +46,20 @@ void OrderPrecedence()
     Console.WriteLine(h);
 }
 
-int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
+void TestLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
 
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
 
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
+}
