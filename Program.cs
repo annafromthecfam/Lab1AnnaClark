390ce0471d2e9c054092327252baf6fac9bef470
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 16:57:01 -0600

    finish step: 6

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ int c = 2;
 int d = a + b * c;
 Console.WriteLine(d);
 
+d = (a +b) * c;
+Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
 void WorkWithIntegers()
 {
     int a = 18;
