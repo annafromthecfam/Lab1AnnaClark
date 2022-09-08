18b7ff8a6c6a22f78adf11294d1f9e7925edc17c
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:11:29 -0600

    finished step: 11

--- Program.cs
+++ Program.cs
@@ -56,3 +56,6 @@ Console.WriteLine($"remainder: {e}");
 int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
