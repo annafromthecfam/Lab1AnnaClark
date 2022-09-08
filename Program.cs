2f9608848e8fb796ab877b525c82c3bc9921c9c8
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:09:50 -0600

    finished step: 10

--- Program.cs
+++ Program.cs
@@ -52,3 +52,7 @@ int d = (a + b) / c;
 int e = (a + b) % c;
 Console.WriteLine($"quotient: {d}");
 Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
