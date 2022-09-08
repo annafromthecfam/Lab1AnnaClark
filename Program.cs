d9d390dfce34120c030890ee736a24c759a417ed
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:03:09 -0600

    finished step: 8

--- Program.cs
+++ Program.cs
@@ -1,23 +1,5 @@
 ﻿//WorkWithIntegers();
-
-int a = 5;
-int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-
-d = (a +b) * c;
-Console.WriteLine(d);
-
-d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-
-int e = 7;
-int f = 4;
-int g = 3;
-int h = (e + f) / g;
-Console.WriteLine(h);
-
+OrderPrecedence();
 
 void WorkWithIntegers()
 {
@@ -40,4 +22,25 @@ void WorkWithIntegers()
     // experiment
     c = a + b - 12 * 17;
     Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a +b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
 }
