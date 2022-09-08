477578a39e034916740420374b5365a5244975f6
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:07:24 -0600

    finished step: 9

--- Program.cs
+++ Program.cs
@@ -44,3 +44,11 @@ void OrderPrecedence()
     int h = (e + f) / g;
     Console.WriteLine(h);
 }
+
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
