0a321b962d328d2dfdef457db078d4efc7810c72
annafromthecfam <anna@myclarkfamily.org>
2022-09-08 17:32:04 -0600

    finished step: 20

--- Program.cs
+++ Program.cs
@@ -93,4 +93,11 @@ void WorkWithDoubles()
     decimal min = decimal.MinValue;
     decimal max = decimal.MaxValue;
     Console.WriteLine($"The range of the decimal type is {min} to {max}");
-    
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
